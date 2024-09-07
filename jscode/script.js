bb9cdd171dd1e7987b8eeb3a125d6fba99073fb9
LeenaMondal08 <leenamondal1813@gmail.com>
2024-09-07 22:44:57 +0530

    chenge

--- jscode/script.js
+++ jscode/script.js
@@ -1,3 +1,3 @@
 console.log("hello js"); //show the otput on colsole
 document.write("documenting my js journey");// show the output directly on the web pages
-alert("carefull")//show a poped up msg on browser
+alert("carefull");//show a poped up msg on browser
