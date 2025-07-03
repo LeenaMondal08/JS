a96b65d0bbd639157453ad9e993f64dc00d2a284
LeenaMondal08 <leenamondal1813@gmail.com>
2025-07-03 23:32:45 +0530

    promptpractice

---
+++ promtfunction/Practice2.js
@@ -0,0 +1,6 @@
+let guessNum=6;
+let userNum=prompt("guess the game number");
+while(guessNum!=userNum){
+    let userNum=prompt("guess the game number correctly");
+}
+console.log("congrats!you enter the write number");
