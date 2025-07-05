a94238e11eb315a5dae128d5c151f6041b46ba29
LeenaMondal08 <leenamondal1813@gmail.com>
2025-07-05 07:57:33 +0530

    window object intro

---
+++ JS_DOM/Window_object/script.js
@@ -0,0 +1,8 @@
+console.log("hello");
+alert("welcome");
+console.log(window);
+window.console.log("leena");// console.log();
+window.alert("hello");
+console.dir(document.body);//print the object
+console.log(document.body);//print the html code for body
+console.dir(document.body.chileNodes[2]);
