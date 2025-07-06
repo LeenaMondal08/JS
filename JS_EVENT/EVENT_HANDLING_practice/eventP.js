668d0363eaa1c0fee99605bfb824c5c88612b3d2
LeenaMondal08 <leenamondal1813@gmail.com>
2025-07-06 12:16:45 +0530

    practice_event

--- JS_EVENT/EVENT_HANDLING_practice/eventP.js
+++ JS_EVENT/EVENT_HANDLING_practice/eventP.js
@@ -1,7 +1,13 @@
-let btn=document.querySelector("button");
-btn.addEventListener("onclick",()=>{
-    btn.style.backgroundColor="black";
-})
-btn.addEventListener("onclick",()=>{
-    btn.style.backgroundColor="white";
-})
+let btn=document.querySelector("#mode");
+let currMode="light";
+btn.addEventListener("click",()=>{
+    if(currMode==="light"){
+    currMode="dark";
+    document.querySelector("body").style.backgroundColor="black";
+    }
+    else{
+    currMode="light";
+    document.querySelector("body").style.backgroundColor="white";
+    }
+    console.log(currMode);
+});
